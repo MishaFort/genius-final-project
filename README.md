147f7cd92a0e6c5c0fa1a22c271e19b71c939409
MishaFort <fortunenko2011@yahoo.com>
2024-02-17 16:34:27 +0200

    Almost done with modal. 17.02.2024

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project-1-GeniusSpace-Bakery
+Project 1 (GeniusSpace) Пекарня
---
+++ js/modal.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector('.backdrop');
+const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnClose = document.querySelector('.modal-btn-close');
+
+const modalBtnOpenFooter = document.querySelector(".footer-modal-btn-open");
+
+const toggleModal = () => modal.classList.toggle('is-hidden');
+
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);
+
+modalBtnOpenFooter.addEventListener("click", toggleModal);
