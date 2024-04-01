1d9b2e899fa156f120963f7640aabf2c88e122df
MishaFort <fortunenko2011@yahoo.com>
2024-04-01 17:14:40 +0300

    Almost done, js ne dyshi

--- js/page-nav.js
+++ js/page-nav.js
@@ -4,45 +4,45 @@ let nextPage = 2;
 
 function showPage(page) {
   // Hide all pages
-  var pages = document.getElementsByClassName('page');
+  var pages = document.getElementsByClassName('page-grid');
   for (var i = 0; i < pages.length; i++) {
     pages[i].style.display = 'none';
   }
   // Show the selected page
-  var selectedPage = document.getElementsByClassName('page')[page - 1];
-  selectedPage.style.display = 'flex';
+  var selectedPage = document.getElementsByClassName('page-grid')[page - 1];
+  selectedPage.style.display = 'grid';
 
   // Remove the active class from all buttons
-  const buttons = document.querySelectorAll('.navigation__button');
+  const buttons = document.querySelectorAll('.page-navigation__button');
   buttons.forEach(button => {
-    button.classList.remove('navigation__button--active');
+    button.classList.remove('page-navigation__button--active');
   });
 
   // Add the active class to the button for the current page
   const currentButton = document.querySelector(
-    `.navigation__button[onclick="showPage(${page})"]`
+    `.page-navigation__button[onclick="showPage(${page})"]`
   );
-  currentButton.classList.add('navigation__button--active');
+  currentButton.classList.add('page-navigation__button--active');
 
   // Disable the "Previous" button if we're on the first page
   if (page === 1) {
     document
-      .querySelector('.navigation__button[onclick="showPage(prevPage)"]')
+      .querySelector('.page-navigation__button[onclick="showPage(prevPage)"]')
       .classList.add('disabled');
   } else {
     document
-      .querySelector('.navigation__button[onclick="showPage(prevPage)"]')
+      .querySelector('.page-navigation__button[onclick="showPage(prevPage)"]')
       .classList.remove('disabled');
   }
 
   // Disable the "Next" button if we're on the last page
   if (page === 8) {
     document
-      .querySelector('.navigation__button[onclick="showPage(nextPage)"]')
+      .querySelector('.page-navigation__button[onclick="showPage(nextPage)"]')
       .classList.add('disabled');
   } else {
     document
-      .querySelector('.navigation__button[onclick="showPage(nextPage)"]')
+      .querySelector('.page-navigation__button[onclick="showPage(nextPage)"]')
       .classList.remove('disabled');
   }
 
